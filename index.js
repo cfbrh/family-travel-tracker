69e7a6512cf566b430c13b098788c30ba90af087
wz2647 <131400434+cfbrh@users.noreply.github.com>
2025-02-20 01:23:39 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,109 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "123456",
+  port: 5433,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let currentUserId = 1;
+let users = [];
+
+//let users = [
+//  { id: 1, name: "Wenjia", color: "powderblue" },
+//  { id: 2, name: "Angela", color: "teal" },
+//];
+
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries v Join users u on u.id=v.user_id where v.user_id=$1;", [currentUserId]);
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+
+async function getCurrentUser() {
+  const result = await db.query("select * from users;")
+  users = result.rows;
+  const currentUser = users.find((user) => user.id == currentUserId);
+  if (!currentUser) {
+    throw new Error("User not found");
+  }
+  return currentUser;
+}
+
+app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
+  const currentUser = await getCurrentUser();
+  res.render("index.ejs", {
+    countries: countries,
+    total: countries.length,
+    users: users, 
+    color: currentUser.color, 
+  });
+});
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+  const currentUser = await getCurrentUser();
+
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+      [input.toLowerCase()]
+    );
+
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
+        [countryCode, currentUserId]
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/user", async (req, res) => {
+  console.log(req.body)
+  if (req.body.add == "new") {
+    res.render("new.ejs");
+  } else {
+    currentUserId = req.body.user
+    res.redirect("/")
+  }
+});
+
+app.post("/new", async (req, res) => {
+  //Hint: The RETURNING keyword can return the data that was inserted.
+  //https://www.postgresql.org/docs/current/dml-returning.html
+  const name = req.body.name;
+  const color = req.body.color;
+  const result = await db.query(
+    "insert into users (name, color) values ($1, $2) RETURNING *;", [name, color],
+  ); 
+  const id = result.rows[0].id;
+  currentUserId = id;
+
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
